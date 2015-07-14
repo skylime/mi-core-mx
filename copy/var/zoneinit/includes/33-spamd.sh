470973f550fd333e7d935f89a13c9f7fba155935
Thomas Merkel <tm@core.io>
2015-07-14 16:32:35 +0200

    version bump to 15.1.0 release

--- copy/var/zoneinit/includes/33-spamd.sh
+++ copy/var/zoneinit/includes/33-spamd.sh
@@ -2,9 +2,6 @@
 CRON='42 7 * * * sudo -u spamd /opt/local/bin/sa-update && kill -SIGHUP $(cat /var/spamassassin/spamd.pid)'
 (crontab -l 2>/dev/null || true; echo "$CRON" ) | sort | uniq | crontab
 
-# Run sa-learn sync to have valid bayes database
-sudo -u spamd sa-learn --sync
-
 # Run pyzor discover
 sudo -u spamd pyzor --homedir /opt/local/etc/spamassassin ping || \
 	sudo -u spamd pyzor --homedir /opt/local/etc/spamassassin discover
