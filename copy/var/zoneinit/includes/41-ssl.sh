0ae9e01cc8d9999029424c5813e6d560fe3e6844
Thomas Merkel <tm@core.io>
2017-02-17 15:39:59 +0100

    fix permissions for ssl certificates

--- copy/var/zoneinit/includes/41-ssl.sh
+++ copy/var/zoneinit/includes/41-ssl.sh
@@ -17,18 +17,24 @@ if mdata-get mx_ssl 1>/dev/null 2>&1; then
 	openssl pkey -in "${SSL_HOME}/exim.pem" -out "${SSL_HOME}/exim.key"
 	openssl crl2pkcs7 -nocrl -certfile "${SSL_HOME}/exim.pem" | \
 		openssl pkcs7 -print_certs -out "${SSL_HOME}/exim.crt"
+	chgrp mail ${SSL_HOME}/exim.*
 	)
 else
 	# Try to generate let's encrypt ssl certificate for the hostname
 	if /opt/core/bin/ssl-letsencrypt.sh 1>/dev/null; then
 		LE_HOME='/opt/local/etc/letsencrypt/'
 		LE_LIVE="${LE_HOME}live/$(hostname)/"
-		# Workaround to link correct files for SSL_HOME
-		ln -s ${LE_LIVE}/fullchain.pem ${SSL_HOME}/exim.crt
-		ln -s ${LE_LIVE}/privkey.pem ${SSL_HOME}/exim.key
+		# Workaround to copy correct files for SSL_HOME
+		(
+		umask 0077
+		cat ${LE_LIVE}/fullchain.pem > ${SSL_HOME}/exim.crt
+		cat ${LE_LIVE}/privkey.pem   > ${SSL_HOME}/exim.key
+		chgrp mail ${SSL_HOME}/exim.*
+		)
 		# Update renew-hook.sh
 		echo '#!/usr/bin/env bash' > ${LE_HOME}renew-hook.sh
-		echo 'svcadm restart svc:/pkgsrc/exim:default' >> ${LE_HOME}renew-hook.sh
+		echo "cat ${LE_LIVE}/fullchain.pem > ${SSL_HOME}/exim.crt" >> ${LE_HOME}renew-hook.sh
+		echo "cat ${LE_LIVE}/privkey.pem   > ${SSL_HOME}/exim.key" >> ${LE_HOME}renew-hook.sh
 	else
 		# Fallback to selfsigned ssl certificates
 		/opt/core/bin/ssl-selfsigned.sh -d ${SSL_HOME} -f exim
