313be442ebdc512d516dbee86f238ca19c7df08a
Thomas Merkel <tm@core.io>
2018-03-14 08:43:28 +0100

    exim-cron script should not fail if db folder doesn't exists

--- copy/var/zoneinit/includes/41-ssl.sh
+++ copy/var/zoneinit/includes/41-ssl.sh
@@ -2,4 +2,8 @@
 # own ssl certificates, let's encrypt and selfsigned fallbacks for dev
 # usage
 
+# Request and manage SSL certificates
 /opt/core/bin/ssl-generator.sh /opt/local/etc/exim/ssl mx_ssl exim svc:/pkgsrc/exim:default
+
+# Fix permissions
+chgrp mail /opt/local/etc/exim/ssl/exim.*
