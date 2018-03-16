a474ddca7f783a0eb19ab0c503ec7d86f952cda6
Thomas Merkel <tm@core.io>
2018-03-16 18:09:28 +0100

    Remove spamassassin and clamav, replaced by rspamd

--- copy/var/zoneinit/includes/32-clamd.sh
+++
@@ -1,6 +0,0 @@
-# run freshclamd once on provisioning
-/opt/local/bin/freshclam
-
-# enable clamav services
-/usr/sbin/svcadm enable svc:/pkgsrc/clamav:clamd
-/usr/sbin/svcadm enable svc:/pkgsrc/clamav:freshclamd
