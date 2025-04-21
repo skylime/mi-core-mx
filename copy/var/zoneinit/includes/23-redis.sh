880063d2a1ce2eaf3170b3415bce00972171a23a
Thomas Merkel <tm@core.io>
2025-04-21 22:50:32 +0200

    feat: security improvements for redis usage
    Switch to redis sockets with correct handling in exim and rspamd.
    Provide and use extra user per service.

--- copy/var/zoneinit/includes/23-redis.sh
+++ copy/var/zoneinit/includes/23-redis.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 # Configure redis for replication and rspamd services
 
+chmod o+x /var/db/redis
+chmod 775 /var/log/redis
+
+chown mail /var/db/redis/api-repl
+chown rspamd /var/db/redis/rspamd
+
 svcadm enable svc:/core/redis:api-repl
 svcadm enable svc:/core/redis:rspamd
-
-
