6d31d4fbc5a652c7ac83bfabe9e073a10e220022
Thomas Merkel <tm@core.io>
2025-04-21 22:52:33 +0200

    feat: rspamd version 3.11.1, logadm and disable greylist

--- copy/var/zoneinit/includes/61-logadm.sh
+++ copy/var/zoneinit/includes/61-logadm.sh
@@ -1,6 +1,7 @@
 # Extra logadm rules for all required mail services
 
-logadm -w '/var/log/exim/main'        -A 7d -p 1d -c -N -o mail -g mail -m 640
-logadm -w '/var/log/exim/reject'      -A 7d -p 1d -c -N -o mail -g mail -m 640
-logadm -w '/var/log/redis/redis.log'  -A 7d -p 1d -c -N -o redis -g redis -m 640
+logadm -w '/var/log/exim/main' -A 7d -p 1d -c -N -o mail -g mail -m 640
+logadm -w '/var/log/exim/reject' -A 7d -p 1d -c -N -o mail -g mail -m 640
+logadm -w '/var/log/redis/redis.log' -A 7d -p 1d -c -N -o redis -g redis -m 640
 logadm -w '/var/log/clamav/clamd.log' -A 7d -p 1d -c -N -o clamav -g clamav -m 640
+logadm -w '/var/log/rspamd/rspamd.log' -A 7d -p 1d -c -N -o rspamd -g rspamd -m 640
