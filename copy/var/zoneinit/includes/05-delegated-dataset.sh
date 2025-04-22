779df55ae6130e5989a805d51765fc21065cf364
Thomas Merkel <tm@core.io>
2025-04-22 22:48:35 +0200

    feat: delegate dataset for rspamd and exim mailq

--- copy/var/zoneinit/includes/05-delegated-dataset.sh
+++ copy/var/zoneinit/includes/05-delegated-dataset.sh
@@ -1,10 +1,13 @@
-#!/bin/bash
-UUID=$(mdata-get sdc:uuid)
-DDS=zones/${UUID}/data
+#!/usr/bin/env bash
 
-if zfs list ${DDS} 1>/dev/null 2>&1; then
-	zfs create ${DDS}/rspamd || true
-	if ! zfs get -o value -H mountpoint ${DDS}/rspamd | grep -q /var/db/redis/rspamd; then
-		zfs set mountpoint=/var/db/redis/rspamd ${DDS}/rspamd
-	fi
+if DDS=$(/opt/core/bin/dds); then
+  zfs create "${DDS}/rspamd" || true
+  if ! zfs get -o value -H mountpoint "${DDS}/rspamd" | grep -q /var/db/redis/rspamd; then
+    zfs set mountpoint=/var/db/redis/rspamd "${DDS}/rspamd"
+  fi
+
+  zfs create "${DDS}/exim" || true
+  if ! zfs get -o value -H mountpoint "${DDS}/exim" | grep -q /var/spool/exim; then
+    zfs set mountpoint=/var/spool/exim "${DDS}/exim"
+  fi
 fi
